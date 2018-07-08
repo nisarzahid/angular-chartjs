764c2d12f6334db598faf0c8823a688e0bd0c214
zahid-nisar-au <nisarzahid@gmail.com>
2018-07-08 23:32:34 +1000

    Added weather service

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -9,7 +9,7 @@ export class WeatherService {
   constructor(private _http: HttpClient) { }
 
   dailyForecast() {
-    return this._http.get("http://samples.openweathermap.org/data/2.5/history/city?q=Warren,OH&appid=b6907d289e10d714a6e88b30761fae22")
+    return this._http.get("http://samples.openweathermap.org/data/2.5/history/city?q=Warren,OH&appid=97b0669093329d52e9118982001755cc")
       .map(result => result);
   }
 
